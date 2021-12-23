bf533ca7396afd3a4d2eb6e4fc0913d2618deaa2
zdm <zdm@softvisio.net>
2021-12-23 04:27:29 +0200

    fix: backend id selector

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 EXTENSION = softvisio_locks
 DATA =	\
 	softvisio_locks--1.0.0.sql \
-	softvisio_locks--1.0.0--1.0.1.sql
+	softvisio_locks--1.0.0--1.0.1.sql \
+	softvisio_locks--1.0.1--1.0.2.sql
 
 PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
---
+++ softvisio_locks--1.0.1--1.0.2.sql
@@ -0,0 +1,27 @@
+\echo 'You need to use the following commands:'
+\echo 'CREATE EXTENSION IF NOT EXISTS softvisio_locks CASCADE;'
+\echo 'ALTER EXTENSION softvisio_locks UPDATE;'
+\echo \quit
+
+CREATE OR REPLACE VIEW pg_active_backend AS (
+    SELECT
+        ( extract( EPOCH FROM backend_start ) * 1000000 )::int8 AS id,
+        pid
+    FROM
+        pg_stat_activity
+    WHERE
+        datname = current_database()
+        AND backend_start IS NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION pg_backend_id () RETURNS int8 STABLE AS $$
+BEGIN
+    RETURN ( SELECT id FROM pg_active_backend WHERE pid = pg_backend_pid() );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION pg_backend_id ( _pid int4 ) RETURNS int8 STABLE AS $$
+BEGIN
+    RETURN ( SELECT id FRON pg_active_backend WHERE pid = _pid );
+END;
+$$ LANGUAGE plpgsql;
